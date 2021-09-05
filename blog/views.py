de00d5937ad5cfd372f1657b4dce7c773921bdaa
prasannabiswas <probiswas82@gmail.com>
2021-09-06 04:50:35 +0530

    Made changes to blog

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def all_blogs(request):
-    blogs = Blog.objects.order_by('-date')[:5]
+    # blog_count = Blog.objects.count we had to pass this for the accurate no of blogs present
+    blogs = Blog.objects.order_by('-date') #[:5] if we wanted only the first five
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
 
 def detail(request, blog_id):
